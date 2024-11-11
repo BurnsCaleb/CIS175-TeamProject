9ae5ad7bae37c956682a7b3b69f5283fe26b8f06
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-11-10 19:38:37 -0600

    Update RequiredFieldTags.java

--- src/java/music/tags/RequiredFieldTags.java
+++ src/java/music/tags/RequiredFieldTags.java
@@ -10,15 +10,23 @@ import javax.servlet.jsp.JspException;
 //created custom jsp tag 
 public class RequiredFieldTags extends SimpleTagSupport{
     private String fieldName;
+    private String error;
 //sets field name of the product page with the required field name
     public void setFieldName(String fieldName){
         this.fieldName = fieldName;
+    }
+    
+     public void setError(String error) {
+        this.error = error;
     }
     //checks to see if the fieldname is set and not empty then writes the field name followed by the asterisk on the required fields
     @Override
     public void doTag() throws JspException, IOException{
         if (fieldName != null && !fieldName.isEmpty()){
-          getJspContext().getOut().write(fieldName + " *");
+          getJspContext().getOut().write(fieldName);
+        }
+        if(error != null && !error.isEmpty()){
+            getJspContext().getOut().write("<div class='error'> *" + error + "</div>");
         }
         
     }
